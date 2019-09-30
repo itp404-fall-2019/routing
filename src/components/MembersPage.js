1deaf8b95d5de98f898f0dca533ec6ae6653dba2
David Tang <dtang@auditboard.com>
2019-09-29 18:03:24 -0700

    update

--- src/components/MembersPage.js
+++ src/components/MembersPage.js
@@ -3,7 +3,7 @@ import MemberImage from './MemberImage';
 import MemberDetails from './MemberDetails';
 import Loading from './Loading';
 import { fetchMembers } from './../GitHubApi';
-import { Route, Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 export default class MembersPage extends React.Component {
   constructor(props) {
@@ -18,20 +18,27 @@ export default class MembersPage extends React.Component {
     this.setState({ members, loading: false });
   }
   render() {
+    const { match } = this.props;
+    
     return (
       <div>
         {this.state.loading ? <Loading /> : (
           <div className="members">
             {this.state.members.map((member) => {
               return (
-                <Link to={`/orgs/facebook/members/${member.login}`} key={member.id}>
+                <Link to={`${match.path}/${member.login}`} key={member.id}>
                   <MemberImage member={member} />
                 </Link>
               );
             })}
           </div>
         )}
-        <Route path={`/orgs/facebook/members/:login`} component={MemberDetails} />
+        <Switch>
+          <Route path={`${match.path}/:login`} component={MemberDetails} />
+          <Route path={match.path}>
+            <h3>Please select a member.</h3>
+          </Route>
+        </Switch>
       </div>
     );
   }
